463de8f862cb1b5a9fa096764a9c88bee17c011e
Jørgen Blakstad <Jorgen.Blakstad@datametrix.no>
2017-05-24 16:37:32 +0200

    soon to be 1st release

--- rancher_env/__init__.py
+++ rancher_env/__init__.py
@@ -1,1 +1,1 @@
-from .rancher_cli_commands import list_all_environments, create_config, list_configs, create_config, switch_config, make_confdir
+from .rancher_cli_commands import list_all_environments, create_config, list_configs, create_config, switch_config, make_confdir, list_all_containers
--- rancher_env/rancher_cli_commands.py
+++ rancher_env/rancher_cli_commands.py
@@ -10,16 +10,83 @@ def get_environments():
     '''
     Gets environments in current configuration
     '''
-    proc = subprocess.Popen(['rancher', 'env', 'ls'],stdout=subprocess.PIPE)
+    proc = subprocess.Popen(['rancher', 'env', 'ls', '--format', 'json'],stdout=subprocess.PIPE)
     output, error = proc.communicate()
     lines = output.splitlines()
     result = []
-    counter = 0
     for line in lines:
-        if not counter == 0:
-            result.append(line.decode("utf-8"))
-        counter += 1
+        result.append(line.decode("utf-8"))
     return result
+
+def get_containers():
+    '''
+    Gets the containers in the current configuration
+    '''
+    proc = subprocess.Popen(['rancher', 'ps', '--format', 'json'],stdout=subprocess.PIPE)
+    output, error = proc.communicate()
+    lines = output.splitlines()
+    result = []
+    for line in lines:
+        result.append(line.decode("utf-8"))
+    return result
+
+def get_containers_advanced(environment, url, accesskey, secretkey):
+    '''
+    Gets the containers in the current configuration
+    '''
+    proc = subprocess.Popen(['rancher', '--url', url, '--access-key', accesskey, '--secret-key', secretkey, '-env', environment, 'ps', '--format', 'json'],stdout=subprocess.PIPE)
+    output, error = proc.communicate()
+    lines = output.splitlines()
+    result = []
+    for line in lines:
+        result.append(line.decode("utf-8"))
+    return result
+
+def tablefy_environments(table, environments):
+    for environment in environments:
+        row = []
+        row.append(environment["Environment"]["id"])
+        row.append(environment["Environment"]["name"])
+        row.append(environment["Environment"]["orchestration"])
+        row.append(environment["Environment"]["state"])
+        row.append(environment["Environment"]["created"])
+        row.append(environment["url"])
+        table.append_row(row)
+    return table
+
+def tablefy_containers(table, containers):
+    for container in containers:
+        row = []
+        row.append(container["ID"])
+        row.append(container["Service"]["type"])
+        row.append(container["Name"])
+        image = "N/A"
+        #stack = container["Stack"]
+        #if "dockerCompose" in stack:
+        #    dockerCompose = stack["dockerCompose"].split("\n")
+        #    for line in dockerCompose:
+        #        if "image" in line:
+        #            image = line.split(":")[1].strip()
+        #            break
+        try:
+            image = container["Service"]["launchConfig"]["imageUuid"]
+        except:
+            print("not found")
+        row.append(image)
+        row.append(container["Service"]["state"])
+        try:
+            nContainers = len(container["Service"]["instanceIds"])
+            scale = container["Service"]["scale"]
+            row.append(str(nContainers)+"/"+str(scale))
+        except:
+            row.append("N/A")
+        row.append("N/A")
+        row.append("N/A")
+        row.append("N/A")
+        row.append(container["env"])
+        row.append(container["url"])
+        table.append_row(row)
+    return table
 
 def get_current_config():
     '''
@@ -53,33 +120,76 @@ def get_saved_configs():
     return result
 
 
-def list_all_environments():
+def list_all_containers():
+    containers = get_all_containers()
+    #for container in containers:
+    #    print(container["Stack"]["dockerCompose"])
+
+    table = BeautifulTable(max_width=200, default_alignment=BeautifulTable.ALIGN_LEFT)
+    table.width_exceed_policy = BeautifulTable.WEP_ELLIPSIS
+    table.column_headers = ["ID", "TYPE", "NAME", "IMAGE", "STATE", "SCALE", "SYSTEM", "ENDPOINTS", "DETAIL", "ENVIRONMENT", "URL"]
+    table.left_border_char = ''
+    table.right_border_char = ''
+    table.top_border_char = ''
+    table.bottom_border_char = ''
+    table.header_seperator_char = ''
+    table.row_seperator_char = ''
+    table.intersection_char = ''
+    table.column_seperator_char = ''
+    # # table.right_padding_widths['NAME'] = 2
+    # # table.right_padding_widths['ORCHESTRATION'] = 2
+    # # table.right_padding_widths['STATE'] = 3
+
+    tablefy_containers(table,containers)
+
+    # Print final table
+    print(table)
+
+def get_all_containers():
     '''
-    List all environments in .rancher and .rancher-env
+    Function that gets all the containers/servises and returns them structured
     '''
 
-    table = BeautifulTable(max_width=160, default_alignment=BeautifulTable.ALIGN_LEFT)
-    #table = BeautifulTable(default_alignment=BeautifulTable.ALIGN_LEFT)
-    #table.width_exceed_policy = BeautifulTable.WEP_WRAP
-    #table.auto_calculate_width()
-    table.column_headers = ["URL", "ID", "NAME", "ORCHESTRATION", "STATE", "CREATED"]
+    environments = get_all_environments()        
+    current_config = get_current_config()
+    saved_configs = get_saved_configs()
+    containers = []
+
+    for environment in environments:
+        config = {}
+        if environment["url"] == current_config["url"]:
+            config = current_config
+        else:
+            for saved_config in saved_configs:
+                if environment["url"] == saved_config["url"]:
+                    config = saved_config
+                    break
+        for container in get_containers_advanced(environment["ID"], environment["url"], config["accessKey"], config["secretKey"]):
+            container = json.loads(container)
+            container["env"] = environment["ID"]
+            container["url"] = environment["url"]
+            containers.append(container) 
+
+    return containers
+
+def get_all_environments():
+    '''
+    Function that gets all the environments and returns them structured
+    '''
 
     current_config = get_current_config()
     saved_configs = get_saved_configs()
-
+    environments = []
     # First check if current config is one of the saved ones
     current_saved = False
     for config in saved_configs:
         if config["url"] == current_config["url"]:
             current_saved = True
     if not current_saved:
-        environments = get_environments()
-        for environment in environments:
-            row = [current_config['url']] + environment.split()
-            print(len(row))
-            print(row)
-            table.append_row(row)
-
+        for env in get_environments():
+            env = json.loads(env)
+            env["url"] = current_config["url"]
+            environments.append(env)
     # Backup current config, and remove it
     current_config_file_exists = True
     if os.path.isfile(current_config["path"]):
@@ -91,19 +201,43 @@ def list_all_environments():
     # Iterate each saved config
     for config in saved_configs:
         copyfile(config["path"], current_config["path"])
-        environments = get_environments()
-        for environment in environments:
-            row = [config['url']] + environment.split("\t")
-            table.append_row(row)
+        for env in get_environments():
+            env = json.loads(env)
+            env["url"] = config["url"]
+            environments.append(env)
         os.remove(current_config["path"])
-
-    # Print final table
-    print(table)
 
     # Revert to old config if exists
     if current_config_file_exists:
         os.rename(current_config["path"] + ".bak", current_config["path"])
 
+    # Return all environments
+    return environments
+
+def list_all_environments():
+    '''
+    List all environments in .rancher and .rancher-env
+    '''
+
+    table = BeautifulTable(max_width=160, default_alignment=BeautifulTable.ALIGN_LEFT)
+    table.column_headers = ["ID", "NAME", "ORCHESTRATION", "STATE", "CREATED", "URL"]
+    table.left_border_char = ''
+    table.right_border_char = ''
+    table.top_border_char = ''
+    table.bottom_border_char = ''
+    table.header_seperator_char = ''
+    table.row_seperator_char = ''
+    table.intersection_char = ''
+    table.column_seperator_char = ''
+    # table.right_padding_widths['NAME'] = 2
+    # table.right_padding_widths['ORCHESTRATION'] = 2
+    # table.right_padding_widths['STATE'] = 3
+
+    environments = get_all_environments()
+    tablefy_environments(table,environments)
+
+    # Print final table
+    print(table)
 
 
 def create_config(name):
